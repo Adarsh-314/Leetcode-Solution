a858840825f8ad01da744736996af45b895bfe48
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-07-04 12:05:02 +0530

    Time: 0 ms (100%), Space: 27.2 MB (35.13%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(std::vector<int>& height) {
+        int total_water = 0;
+        std::stack<int> st;         
+        for (int i = 0; i < height.size(); i++) {
+            while (!st.empty() && height[i] > height[st.top()]) {
+                int bottom_index = st.top();
+                st.pop();
+                if (st.empty()) {
+                    break; 
+                }
+                int left_index = st.top();
+                int width = i - left_index - 1;
+                int bounded_height = std::min(height[i], height[left_index]) - height[bottom_index];
+                total_water += width * bounded_height;
+            }
+            st.push(i);
+        }
+        
+        return total_water;
+    }
+};
