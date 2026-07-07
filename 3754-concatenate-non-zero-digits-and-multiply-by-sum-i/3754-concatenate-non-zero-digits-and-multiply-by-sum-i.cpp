e2c481411231ee3786982bae5f781b7f08fd78c4
Adarsh-314 <adarshp.ug25.me@nitp.ac.in>
2026-07-07 12:30:45 +0530

    Time: 0 ms (100%), Space: 9 MB (74.01%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    long long formNumber(vector<int>& v) {
-        long long number = 0;
-       for (int i = v.size() - 1; i >= 0; i--) {
-            number = number * 10 + v[i]; 
-        }
-        return number;
-    }
     long long sumAndMultiply(int n) {
-        long long ans;
-        vector<int> v;
-        long long k = n;
-        long long x;
         long long sum = 0;
-        while (k > 0) {
-            if (k%10 != 0) {
-                v.push_back(k % 10);
+        long long x = 0;
+        long long placeValue = 1;
+
+        while (n > 0) {
+            long long digit = n % 10;
+            n /= 10;
+
+            sum += digit;
+
+            if (digit != 0) {
+                x = x + (digit * placeValue);
+                placeValue *= 10;
             }
-            sum += k % 10;
-            k /= 10;
         }
-        x = formNumber(v);
-        ans = x * sum;
-        return ans;
+
+        return x * sum;
     }
 };
